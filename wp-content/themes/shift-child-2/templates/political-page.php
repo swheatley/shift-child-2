aeaf5cd3a1a1299db638d5baff9dcebe2dfa1d49
swheatley <shaylee1@me.com>
2018-01-19 16:48:36 -0700

    Re-styled Political Candidate Website package page

--- wp-content/themes/shift-child-2/templates/political-page.php
+++ wp-content/themes/shift-child-2/templates/political-page.php
@@ -5,31 +5,163 @@
 
 get_header(); ?>
 
-<div id="loop-container" class="loop-container">
-	<?php
-	if ( have_posts() ) :
-		while ( have_posts() ) :
-			the_post(); ?>
-			<div <?php post_class(); ?>>
-				<?php do_action( 'ct_shift_page_before' ); ?>
-				<article>
-					<div class='post-header'>
-						<h1 class='post-title'><?php the_title(); ?></h1>
+<div id="loop-container-political" class="loop-container-political">
+	
+					<div class="post-content-political">
+					<div class="political-header">
+					<figure><img src="https://www.shaylee-hansen.local/wp-content/uploads/2018/01/america-2025465_1280.png"></figure>
+					<h2>Website Packages</h2>	
+					</div>	
+
+					<div class="candidate-packages">
+					<div class="package-one">
+					<div class="header-box">
+					<h2>"First Timer" Package:<span> $500</span> </h2>	
 					</div>
-					<?php ct_shift_featured_image(); ?>
-					<div class="post-content">
-						<?php the_content(); ?>	
-							<?php wp_link_pages( array(
-							'before' => '<p class="singular-pagination">' . __( 'Pages:', 'shift' ),
-							'after'  => '</p>',
-						) ); ?>
-						<?php do_action( 'ct_shift_page_after' ); ?>			
+					<figure>
+					<img src="https://www.shayleehansen.com/wp-content/uploads/2018/01/candidate-basic-package.jpg">
+					</figure>
+					<button class="info-button">Click Here</button>
+					<ul class="site-setup">
+					<h4>Site Setup</h4>
+					<li>Domain Name ( annual )</li>
+					<li>Hosting ( website launch through - November 2018 )</li>
+					<ul class="design">
+					<h4>Design </h4>
+					<ul>
+					<p>All themes are created using Divi to ensure unique customization for the candidate and easy access to content updates for campaign team.</p>
+					<h4>Landing Page: </h4>
+					<li>About Me Section</li>
+					<li>Contact Me Section</li>
+					<ul>
+					<li> Form integration includes email configuration</li>
+					</ul>
+					<li> Volunteer Section</li>
+					<ul>
+					<li> Form integration includes Zapier, Twilio &amp; email configuration</li>
+					</ul>
+					<li> ActBlue Integration links</li>
+					<li> Vote Utah link integration</li>
+					<li> Social media configuration, i.e Twitter &amp; Facebook feed</li>
+					<li> 3 - 5 patriotic images including optimization</li>
+					</ul>
+					</ul>
+					<br>
+					<ul class="candidate-info">
+					<h4>What is needed from the candidate:</h4>
+					<li> Assets</li>
+					<li> Logo</li>
+					<li> Colors
+					<li> Typography</li>
+					<li> Videos</li>
+					<li> Images ( high resolution only )</li>
+					<li> Content</li>
+					</ul>
+					</ul>
+					</ul>
+						
 					</div>
-				</article>	
+					<div class="package-two">
+					<div class="header-box">
+					<h2>"Basic" Package: <span> $1500.00</span> </h2>
+					</div>
+					<figure>
+					<img src="https://www.shayleehansen.com/wp-content/uploads/2018/01/starter-candidate-package.jpg">
+					</figure>
+					<button class="info-button">Click Here</button>
+					<ul class="site-setup">
+					<h4>Site Setup</h4>
+					<li>Domain Name ( annual )</li>
+					<li>Hosting ( website launch through - November 2018 )</li>
+					<ul class="design">
+					<h4>Design </h4>
+					<ul>
+					<p>All themes are created using Divi to ensure unique customization for the candidate and easy access to content updates for campaign team.</p>
+					<h4>Landing Page: </h4>
+					<li>About Me Section</li>
+					<li>Contact Me Section</li>
+					<ul>
+					<li> Form integration includes email configuration</li>
+					</ul>
+					<li> Volunteer Section</li>
+					<ul>
+					<li> Form integration includes Zapier, Twilio &amp; email configuration</li>
+					</ul>
+					<li> ActBlue Integration links</li>
+					<li> Vote Utah link integration</li>
+					<li> Social media configuration, i.e Twitter &amp; Facebook feed</li>
+					<li> 3 - 5 patriotic images including optimization</li>
+					</ul>
+					</ul>
+					<br>
+					<ul class="candidate-info">
+					<h4>What is needed from the candidate:</h4>
+					<li> Assets</li>
+					<li> Logo</li>
+					<li> Colors
+					<li> Typography</li>
+					<li> Videos</li>
+					<li> Images ( high resolution only )</li>
+					<li> Content</li>
+					</ul>
+					</ul>
+					</ul>
 					
-			</div>		
-		<?php endwhile;
-	endif; ?>
+						
+					</div>
+
+					<div class="package-three">
+					<div class="header-box">
+					<h2>"The Works” Package:<span> $3000.00</span></h2>
+					</div>	
+					<figure>
+					<img src="https://www.shayleehansen.com/wp-content/uploads/2018/01/candidate-the-works-package.jpg">
+					</figure>
+					<button class="info-button">Click Here</button>
+					<ul class="site-setup">
+					<h4>Site Setup</h4>
+					<li>Domain Name ( annual )</li>
+					<li>Hosting ( website launch through - November 2018 )</li>
+					<ul class="design">
+					<h4>Design </h4>
+					<ul>
+					<p>All themes are created using Divi to ensure unique customization for the candidate and easy access to content updates for campaign team.</p>
+					<h4>Landing Page: </h4>
+					<li>About Me Section</li>
+					<li>Contact Me Section</li>
+					<ul>
+					<li> Form integration includes email configuration</li>
+					</ul>
+					<li> Volunteer Section</li>
+					<ul>
+					<li> Form integration includes Zapier, Twilio &amp; email configuration</li>
+					</ul>
+					<li> ActBlue Integration links</li>
+					<li> Vote Utah link integration</li>
+					<li> Social media configuration, i.e Twitter &amp; Facebook feed</li>
+					<li> 3 - 5 patriotic images including optimization</li>
+					</ul>
+					</ul>
+					<br>
+					<ul class="candidate-info">
+					<h4>What is needed from the candidate:</h4>
+					<li> Assets</li>
+					<li> Logo</li>
+					<li> Colors
+					<li> Typography</li>
+					<li> Videos</li>
+					<li> Images ( high resolution only )</li>
+					<li> Content</li>
+					</ul>
+					</ul>
+					</ul>
+					</div>
+					</div>
+					</div>
+				
+				
+		
+
 </div>
 
 <?php get_footer();
