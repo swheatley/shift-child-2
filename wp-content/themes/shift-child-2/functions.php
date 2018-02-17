7d56d3690ce1545607a7ce7885ac7cdf12d54c4b
swheatley <shaylee1@me.com>
2018-02-17 16:28:43 -0700

    Styled social media icons for front page

--- wp-content/themes/shift-child-2/functions.php
+++ wp-content/themes/shift-child-2/functions.php
@@ -1,15 +1,25 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'shift_child_enqueue_styles' );
-	 function shift_child_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
-		   } 
+<?php
 
-	function shift_child_scripts(){	   
-	  wp_register_script( 'child-javascript', get_template_directory_uri() . '/js/child-javascript.js', array( 'jquery' ), 1.1, true );
+/* Enqueue Style Sheets */
+
+	function shift_child_enqueue_styles() {
+		   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+		}
+
+	add_action( 'wp_enqueue_scripts', 'shift_child_enqueue_styles' );
+
+/* Enqueue Javascript files */
+
+/* Not to self for some reason the javascript file only works in the parent theme */
+
+	function shift_child_scripts(){
+	  wp_enqueue_script( 'child-javascript', get_stylesheet_directory_uri() . '/js/child-javascript.js',  array( 'jquery' ), '1.0.0', true );
 	}
 	add_action('wp_enqueue_scripts', 'shift_child_scripts');
-		   
-/* Register Widgets */		   
+
+
+/* Register Widgets */
 if ( ! function_exists( ( 'child_theme_widgets' ) ) ) {
 	function child_theme_widgets() {
         	register_sidebar( array(
@@ -30,7 +40,16 @@ if ( ! function_exists( ( 'child_theme_widgets' ) ) ) {
 			'before_title'  => '<h2 class="widget-title">',
 			'after_title'   => '</h2>'
 		) );
-	
+		register_sidebar( array(
+	'name'          => esc_html__( 'Tech Resources Sidebar', 'shift' ),
+	'id'            => 'tech-resources',
+	'description'   => esc_html__( 'Widgets in this area will be shown in the sidebar next to the main post content', 'shift' ),
+	'before_widget' => '<section id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</section>',
+	'before_title'  => '<h2 class="widget-title">',
+	'after_title'   => '</h2>'
+) );
+
 	}
 }
 
